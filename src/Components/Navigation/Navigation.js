325513083c5dc69c0938cfdd77d15c0955820470
Karishma Saini <ksaini7595@gmail.com>
2021-04-11 13:47:46 -0400

    updated the navigation to include the resume toastify download

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import karishma_resume from '../../Documents/Karishma_2021-04-05.docx';
 
+
+toast.configure()
 
 function NavigationBar () {
    
+    const notify = () => {
+        toast.warning ('Downloading Resume!', 
+        { position: toast.POSITION.BOTTOM_LEFT}
+        )
+      }
 
     return(
 
@@ -17,7 +27,7 @@ function NavigationBar () {
             <Nav.Item><Nav.Link href="/"></Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href="/About">About</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href="/Projects">Projects</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link>Resume</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href={karishma_resume} rel="noopener noreferrer" download onClick={notify}>Resume</Nav.Link></Nav.Item>
             <Nav.Item><Nav.Link href="/Contact" >Contact</Nav.Link></Nav.Item>
 
         </Nav>
